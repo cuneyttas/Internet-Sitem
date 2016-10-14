aab3148905aebdbd5eb81f8df4d1abb7c2fb5bdb
Cüneyt TAŞ <cuneyttas@hotmail.com.tr>
2016-10-15 01:39:39 +0300

    Okuduğum Kitaplar sayfası için veriler eklendi ve o sayfanın tasarımına başlandı.

---
+++ kitap_veriler.php
@@ -0,0 +1,1125 @@
+<?php
+
+$kitaplar = array();
+
+$kitaplar[1] = array(
+
+	"resim" => "",
+	"isim" => "Öğretmenin Not Defteri 1",
+	"yazar" => "Vehbi Vakkasoğlu",
+	"sayfa_no" => "204",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[2] = array(
+
+	"resim" => "",
+	"isim" => "Öğretmenin Not Defteri 2",
+	"yazar" => "Vehbi Vakkasoğlu",
+	"sayfa_no" => "175",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[3] = array(
+
+	"resim" => "",
+	"isim" => "Figan",
+	"yazar" => "Ahmed Günbay Yıldız",
+	"sayfa_no" => "253",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[4] = array(
+
+	"resim" => "",
+	"isim" => "Kalem Sırra Dokundu",
+	"yazar" => "Mehmet Erdoğan",
+	"sayfa_no" => "182",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[5] = array(
+
+	"resim" => "",
+	"isim" => "Hızırla Kol Kola",
+	"yazar" => "Mehmet Erdoğan",
+	"sayfa_no" => "192",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[6] = array(
+
+	"resim" => "",
+	"isim" => "Hayret! Dünya Dönüyor!",
+	"yazar" => "Ali Ünlü",
+	"sayfa_no" => "274",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[7] = array(
+
+	"resim" => "",
+	"isim" => "Kavak Yelleri",
+	"yazar" => "Reşat Nuri Güntekin",
+	"sayfa_no" => "381",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[8] = array(
+
+	"resim" => "",
+	"isim" => "Yahudileri Tanıyalım",
+	"yazar" => "M. Şahap Tan",
+	"sayfa_no" => "187",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[9] = array(
+
+	"resim" => "",
+	"isim" => "Hain Futbol",
+	"yazar" => "Michael Coleman",
+	"sayfa_no" => "160",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[10] = array(
+
+	"resim" => "",
+	"isim" => "Ketçap, Mayonez ve Sindirim",
+	"yazar" => "Nick Arnold",
+	"sayfa_no" => "156",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[11] = array(
+
+	"resim" => "",
+	"isim" => "Paradokslar ve Mantık Oyunları",
+	"yazar" => "Serhan Büyükkeçeci",
+	"sayfa_no" => "160",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[12] = array(
+
+	"resim" => "",
+	"isim" => "Türkiye Çocuk Rehber İlmihali",
+	"yazar" => "Hasan Yavaş",
+	"sayfa_no" => "518",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "?"
+
+);
+
+$kitaplar[13] = array(
+
+	"resim" => "",
+	"isim" => "Ağrı Dağı Efsanesi",
+	"yazar" => "Yaşar Kemal",
+	"sayfa_no" => "113",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2004"
+
+);
+
+$kitaplar[14] = array(
+
+	"resim" => "",
+	"isim" => "Tutuklu Yürek",
+	"yazar" => "Osman Çeviksoy",
+	"sayfa_no" => "128",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2004"
+
+);
+
+$kitaplar[15] = array(
+
+	"resim" => "",
+	"isim" => "Yerim Seni ÖSS",
+	"yazar" => "Erdal Demirkıran",
+	"sayfa_no" => "228",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2006"
+
+);
+
+$kitaplar[16] = array(
+
+	"resim" => "",
+	"isim" => "Sürücü Eğitim Rehberi",
+	"yazar" => "Hatice Tatlı-Hasan İpek",
+	"sayfa_no" => "512",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2007"
+
+);
+
+$kitaplar[17] = array(
+
+	"resim" => "",
+	"isim" => "Doğru ve Güzel Konuşma",
+	"yazar" => "Birol Vural",
+	"sayfa_no" => "320",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2008"
+
+);
+
+$kitaplar[18] = array(
+
+	"resim" => "",
+	"isim" => "Akılalmaz Yapılar",
+	"yazar" => "Michael Cox",
+	"sayfa_no" => "159",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[19] = array(
+
+	"resim" => "",
+	"isim" => "Cerrah",
+	"yazar" => "Tess Gerritsen",
+	"sayfa_no" => "296",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[20] = array(
+
+	"resim" => "",
+	"isim" => "Çırak",
+	"yazar" => "Tess Gerritsen",
+	"sayfa_no" => "390",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[21] = array(
+
+	"resim" => "",
+	"isim" => "Da Vinci Şifresi",
+	"yazar" => "Dan Brown",
+	"sayfa_no" => "495",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[22] = array(
+
+	"resim" => "",
+	"isim" => "İffeti Yaşayanlar",
+	"yazar" => "Said Demirtaş",
+	"sayfa_no" => "139",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[23] = array(
+
+	"resim" => "",
+	"isim" => "Gölgelerle Oyun",
+	"yazar" => "Hülya Yakut Üstündağ",
+	"sayfa_no" => "238",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[24] = array(
+
+	"resim" => "",
+	"isim" => "Sol Ayağım",
+	"yazar" => "Christy Brown",
+	"sayfa_no" => "160",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[25] = array(
+
+	"resim" => "",
+	"isim" => "İnsan Denen Mucize",
+	"yazar" => "Ahmet Yakut",
+	"sayfa_no" => "86",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[26] = array(
+
+	"resim" => "",
+	"isim" => "İslam ve Fizik ",
+	"yazar" => "Şaban Döğen",
+	"sayfa_no" => "163",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[27] = array(
+
+	"resim" => "",
+	"isim" => "Olasılıksız",
+	"yazar" => "Adam Fawer",
+	"sayfa_no" => "472",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[28] = array(
+
+	"resim" => "",
+	"isim" => "Türkleri Anlama Kılavuzu 2",
+	"yazar" => "Zeki Kayahan Coşkun",
+	"sayfa_no" => "155",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[29] = array(
+
+	"resim" => "",
+	"isim" => "Farz Et Ki Öldün",
+	"yazar" => "Ebu Abdullah Haris El-Muhasibi",
+	"sayfa_no" => "96",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2009"
+
+);
+
+$kitaplar[30] = array(
+
+	"resim" => "",
+	"isim" => "Minyeli Abdullah",
+	"yazar" => "Hekimoğlu İsmail",
+	"sayfa_no" => "237",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2010"
+
+);
+
+$kitaplar[31] = array(
+
+	"resim" => "",
+	"isim" => "İki Darbe Arasında",
+	"yazar" => "İskender Pala",
+	"sayfa_no" => "265",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2010"
+
+);
+
+$kitaplar[32] = array(
+
+	"resim" => "",
+	"isim" => "Sözmetre",
+	"yazar" => "Lilay Koradan",
+	"sayfa_no" => "153",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2010"
+
+);
+
+$kitaplar[33] = array(
+
+	"resim" => "",
+	"isim" => "Kur'an-ı Kerim Meali",
+	"yazar" => "Muhammed Hamdi Yazır",
+	"sayfa_no" => "302",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2010"
+
+);
+
+$kitaplar[34] = array(
+
+	"resim" => "",
+	"isim" => "Etkili Konuşma",
+	"yazar" => "Cristina Stuart",
+	"sayfa_no" => "290",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "2 Temmuz 2010"
+
+);
+
+$kitaplar[35] = array(
+
+	"resim" => "",
+	"isim" => "Melekler ve Şeytanlar",
+	"yazar" => "Dan Brown",
+	"sayfa_no" => "575",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "07 Temmuz 2010"
+
+);
+
+$kitaplar[36] = array(
+
+	"resim" => "",
+	"isim" => "Peygamberimizin İnsan Kazanma Metodu",
+	"yazar" => "Mehmed Dikmen",
+	"sayfa_no" => "191",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "07 Temmuz 2010"
+
+);
+
+$kitaplar[37] = array(
+
+	"resim" => "",
+	"isim" => "Alacakaranlık",
+	"yazar" => "Stephenie Meyer",
+	"sayfa_no" => "407",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "14 Temmuz 2010"
+
+);
+
+$kitaplar[38] = array(
+
+	"resim" => "",
+	"isim" => "Fatih Sultan Mehmet ve Fetih",
+	"yazar" => "Mustafa Uslu",
+	"sayfa_no" => "174",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "17 Temmuz 2010"
+
+);
+
+$kitaplar[39] = array(
+
+	"resim" => "",
+	"isim" => "Empati",
+	"yazar" => "Adam Fawer",
+	"sayfa_no" => "639",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "01 Ağustos 2010"
+
+);
+
+$kitaplar[40] = array(
+
+	"resim" => "",
+	"isim" => "İlimlerin Diliyle Allah",
+	"yazar" => "Şaban Döğen",
+	"sayfa_no" => "398",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "30 Ağustos 2010"
+
+);
+
+$kitaplar[41] = array(
+
+	"resim" => "",
+	"isim" => "Muhafız",
+	"yazar" => "Selman Kayabaşı",
+	"sayfa_no" => "283",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "02 Eylül 2010"
+
+);
+
+$kitaplar[42] = array(
+
+	"resim" => "",
+	"isim" => "Senin İçin",
+	"yazar" => "Hülya Kartal",
+	"sayfa_no" => "222",
+	"basim_yili" => "2001",
+	"bitis_tarihi" => "09 Eylül 2010"
+
+);
+
+$kitaplar[43] = array(
+
+	"resim" => "",
+	"isim" => "Kayıp Sembol",
+	"yazar" => "Dan Brown",
+	"sayfa_no" => "527",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "17 Eylül 2010"
+
+);
+
+$kitaplar[44] = array(
+
+	"resim" => "",
+	"isim" => "Yeniay",
+	"yazar" => "Stephenie Meyer",
+	"sayfa_no" => "440",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "14 Ekim 2010"
+
+);
+
+$kitaplar[45] = array(
+
+	"resim" => "",
+	"isim" => "Dünya Düzdür",
+	"yazar" => "Thomas Loren Friedman",
+	"sayfa_no" => "459",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "12 Kasım 2010"
+
+);
+
+$kitaplar[46] = array(
+
+	"resim" => "",
+	"isim" => "Çorum Evliyaları",
+	"yazar" => "Ethem Erkoç",
+	"sayfa_no" => "82",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "28 Kasım 2010"
+
+);
+
+$kitaplar[47] = array(
+
+	"resim" => "",
+	"isim" => "Hz. Muhammed Hayatı ve Örnek Kişiliği",
+	"yazar" => "Mehmet Mahfuz Söylemez",
+	"sayfa_no" => "62",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "20 Aralık 2010"
+
+);
+
+$kitaplar[48] = array(
+
+	"resim" => "",
+	"isim" => "2012 Kristal Kafatası",
+	"yazar" => "Manda Scott",
+	"sayfa_no" => "448",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "08 Şubat 2011"
+
+);
+
+$kitaplar[49] = array(
+
+	"resim" => "",
+	"isim" => "Altın Prensipler",
+	"yazar" => "Zübeyir Gündüzalp",
+	"sayfa_no" => "84",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "09 Şubat 2011"
+
+);
+
+$kitaplar[50] = array(
+
+	"resim" => "",
+	"isim" => "Senaryo Romanları",
+	"yazar" => "Necip Fazıl Kısakürek",
+	"sayfa_no" => "357",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "15 Şubat 2011"
+
+);
+
+$kitaplar[51] = array(
+
+	"resim" => "",
+	"isim" => "Gençlerle Söyleşi",
+	"yazar" => "Mehmet Erdoğan",
+	"sayfa_no" => "68",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "18 Şubat 2011"
+
+);
+
+$kitaplar[52] = array(
+
+	"resim" => "",
+	"isim" => "Öldürücü Matematik",
+	"yazar" => "Kjartan Poskitt",
+	"sayfa_no" => "160",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "04 Temmuz 2011"
+
+);
+
+$kitaplar[53] = array(
+
+	"resim" => "",
+	"isim" => "Gözüpek Kâşifler",
+	"yazar" => "Anita Ganeri",
+	"sayfa_no" => "208",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "10 Temmuz 2011"
+
+);
+
+$kitaplar[54] = array(
+
+	"resim" => "",
+	"isim" => "Huzur",
+	"yazar" => "Ahmet Hamdi Tanpınar",
+	"sayfa_no" => "356",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "27 Temmuz 2011"
+
+);
+
+$kitaplar[55] = array(
+
+	"resim" => "",
+	"isim" => "Zamanı Değerlendirme San'atı",
+	"yazar" => "Muhsin Abay",
+	"sayfa_no" => "35",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "30 Temmuz 2011"
+
+);
+
+$kitaplar[56] = array(
+
+	"resim" => "",
+	"isim" => "Cennet Çocukları",
+	"yazar" => "Seyfettin Bulut",
+	"sayfa_no" => "175",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "05 Ağustos 2011"
+
+);
+
+$kitaplar[57] = array(
+
+	"resim" => "",
+	"isim" => "İslâm ve İlim",
+	"yazar" => "Şaban Döğen",
+	"sayfa_no" => "281",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "22 Ekim 2011"
+
+);
+
+$kitaplar[58] = array(
+
+	"resim" => "",
+	"isim" => "2050",
+	"yazar" => "David Passig",
+	"sayfa_no" => "387",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "10 Aralık 2011"
+
+);
+
+$kitaplar[59] = array(
+
+	"resim" => "",
+	"isim" => "İnsanca Yaşamak",
+	"yazar" => "Şaban Döğen",
+	"sayfa_no" => "139",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "15 Ağustos 2012"
+
+);
+
+$kitaplar[60] = array(
+
+	"resim" => "",
+	"isim" => "Alevîlik Bektaşilik Üzerine…",
+	"yazar" => "Mürsel Şahinbaş",
+	"sayfa_no" => "148",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "08 Eylül 2012"
+
+);
+
+$kitaplar[61] = array(
+
+	"resim" => "",
+	"isim" => "Aklından Bir Sayı Tut",
+	"yazar" => "John Verdon",
+	"sayfa_no" => "475",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "17 Eylül 2012"
+
+);
+
+$kitaplar[62] = array(
+
+	"resim" => "",
+	"isim" => "Ülkemizde Cuma Namazı Farzdır",
+	"yazar" => "Mürsel Şahinbaş",
+	"sayfa_no" => "138",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "12 Kasım 2012"
+
+);
+
+$kitaplar[63] = array(
+
+	"resim" => "",
+	"isim" => "Gözlerini Sımsıkı Kapat",
+	"yazar" => "John Verdon",
+	"sayfa_no" => "567",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "02 Nisan 2013"
+
+);
+
+$kitaplar[64] = array(
+
+	"resim" => "",
+	"isim" => "Destanlaşan Çanakkale",
+	"yazar" => "Mustafa Turan",
+	"sayfa_no" => "181",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "30 Temmuz 2013"
+
+);
+
+$kitaplar[65] = array(
+
+	"resim" => "",
+	"isim" => "Dünya Sinema Sanayii",
+	"yazar" => "Giovanni Scognamillo",
+	"sayfa_no" => "251",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "04 Eylül 2013"
+
+);
+
+$kitaplar[66] = array(
+
+	"resim" => "",
+	"isim" => "Mehdix",
+	"yazar" => "Turgay Güler",
+	"sayfa_no" => "236",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "06 Eylül 2013"
+
+);
+
+$kitaplar[67] = array(
+
+	"resim" => "",
+	"isim" => "Sır Küpü",
+	"yazar" => "Turgay Güler",
+	"sayfa_no" => "259",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "14 Eylül 2013"
+
+);
+
+$kitaplar[68] = array(
+
+	"resim" => "",
+	"isim" => "Canlı Bomba",
+	"yazar" => "Yakup Kadri Öztürk",
+	"sayfa_no" => "160",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "17 Eylül 2013"
+
+);
+
+$kitaplar[69] = array(
+
+	"resim" => "",
+	"isim" => "Adnan Menderes",
+	"yazar" => "Veli Sırım",
+	"sayfa_no" => "143",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "19 Eylül 2013"
+
+);
+
+$kitaplar[70] = array(
+
+	"resim" => "",
+	"isim" => "Virüs Saldırıyor",
+	"yazar" => "Wendy Barnaby",
+	"sayfa_no" => "247",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "17 Ekim 2013"
+
+);
+
+$kitaplar[71] = array(
+
+	"resim" => "",
+	"isim" => "Günahkar",
+	"yazar" => "Tess Gerritsen",
+	"sayfa_no" => "409",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "25 Ekim 2013"
+
+);
+
+$kitaplar[72] = array(
+
+	"resim" => "",
+	"isim" => "Kod Adı: Kılıçbalığı",
+	"yazar" => "Mustafa Aydın",
+	"sayfa_no" => "240",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "05 Kasım 2013"
+
+);
+
+$kitaplar[73] = array(
+
+	"resim" => "",
+	"isim" => "Dip Dalga Davutoğlu",
+	"yazar" => "Fatih Bayhan",
+	"sayfa_no" => "243",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "11 Kasım 2013"
+
+);
+
+$kitaplar[74] = array(
+
+	"resim" => "",
+	"isim" => "İkna Odası",
+	"yazar" => "Yıldız Ramazanoğlu",
+	"sayfa_no" => "156",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "19 Kasım 2013"
+
+);
+
+$kitaplar[75] = array(
+
+	"resim" => "",
+	"isim" => "II. Attila",
+	"yazar" => "Kamuran Tümay Yıldız",
+	"sayfa_no" => "488",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "29 Kasım 2013"
+
+);
+
+$kitaplar[76] = array(
+
+	"resim" => "",
+	"isim" => "Kamikaze Operasyonu",
+	"yazar" => "Atilla Akar",
+	"sayfa_no" => "382",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "03 Ocak 2014"
+
+);
+
+$kitaplar[77] = array(
+
+	"resim" => "",
+	"isim" => "Benim Türkiyem",
+	"yazar" => "Elvan Küçük",
+	"sayfa_no" => "440",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "09 Şubat 2014"
+
+);
+
+$kitaplar[78] = array(
+
+	"resim" => "",
+	"isim" => "Huzur Sokağı",
+	"yazar" => "Şule Yüksel Şenler",
+	"sayfa_no" => "552",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "05 Mart 2014"
+
+);
+
+$kitaplar[79] = array(
+
+	"resim" => "",
+	"isim" => "İkiz Bedenler",
+	"yazar" => "Tess Gerritsen",
+	"sayfa_no" => "448",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "21 Mart 2014"
+
+);
+
+$kitaplar[80] = array(
+
+	"resim" => "",
+	"isim" => "Siliniş",
+	"yazar" => "Tess Gerritsen",
+	"sayfa_no" => "464",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "16 Nisan 2014"
+
+);
+
+$kitaplar[81] = array(
+
+	"resim" => "",
+	"isim" => "Şah Mat",
+	"yazar" => "Mario Mazzanti",
+	"sayfa_no" => "512",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "10 Haziran 2014"
+
+);
+
+$kitaplar[82] = array(
+
+	"resim" => "",
+	"isim" => "Cehennem",
+	"yazar" => "Dan Brown",
+	"sayfa_no" => "574",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "03 Ağustos 2014"
+
+);
+
+$kitaplar[83] = array(
+
+	"resim" => "",
+	"isim" => "Simya",
+	"yazar" => "Kubilay Aktaş",
+	"sayfa_no" => "191",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "07 Ağustos 2014"
+
+);
+
+$kitaplar[84] = array(
+
+	"resim" => "",
+	"isim" => "Türkçe'nin Adı Var!",
+	"yazar" => "Sırrı Er",
+	"sayfa_no" => "168",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "11 Ağustos 2014"
+
+);
+
+$kitaplar[85] = array(
+
+	"resim" => "",
+	"isim" => "Örümcekler Ağlarına Neden Yapışmazlar?",
+	"yazar" => "Robert Matthews",
+	"sayfa_no" => "327",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "22 Ağustos 2014"
+
+);
+
+$kitaplar[86] = array(
+
+	"resim" => "",
+	"isim" => "İmamın Manken Kızı",
+	"yazar" => "Emine Şenlikoğlu",
+	"sayfa_no" => "222",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "26 Ağustos 2014"
+
+);
+
+$kitaplar[87] = array(
+
+	"resim" => "",
+	"isim" => "Namazla Yeniden Doğdum",
+	"yazar" => "Yaşar Alptekin",
+	"sayfa_no" => "143",
+	"basim_yili" => "2007",
+	"bitis_tarihi" => "29 Ağustos 2014"
+
+);
+
+$kitaplar[88] = array(
+
+	"resim" => "",
+	"isim" => "Aysel",
+	"yazar" => "Halit Ertuğrul",
+	"sayfa_no" => "182",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "02 Eylül 2014"
+
+);
+
+$kitaplar[89] = array(
+
+	"resim" => "",
+	"isim" => "Kediniz Konuşuyor",
+	"yazar" => "Gülay Kabasakal Ertürk",
+	"sayfa_no" => "79",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "02 Ekim 2014"
+
+);
+
+$kitaplar[90] = array(
+
+	"resim" => "",
+	"isim" => "Şeytanı Uyandırma",
+	"yazar" => "John Verdon",
+	"sayfa_no" => "536",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "12 Ekim 2014"
+
+);
+
+$kitaplar[91] = array(
+
+	"resim" => "",
+	"isim" => "Google Hikâyesi",
+	"yazar" => "David A. Vise, Mark Malseed",
+	"sayfa_no" => "408",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "06 Kasım 2014"
+
+);
+
+$kitaplar[92] = array(
+
+	"resim" => "",
+	"isim" => "Her Güne 1 Ayet Hadis Dua",
+	"yazar" => "Nazlı Hilal Kızılkaya",
+	"sayfa_no" => "401",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "01 Ocak 2015"
+
+);
+
+$kitaplar[93] = array(
+
+	"resim" => "",
+	"isim" => "Celcelutiye, Sufilerin Saklı Hazinesi",
+	"yazar" => "Kubilay Aktaş",
+	"sayfa_no" => "400",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "29 Eylül 2015"
+
+);
+
+$kitaplar[94] = array(
+
+	"resim" => "",
+	"isim" => "Türkiye’nin Ünlüleri",
+	"yazar" => "Önder Güney",
+	"sayfa_no" => "339",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "07 Ekim 2015"
+
+);
+
+$kitaplar[95] = array(
+
+	"resim" => "",
+	"isim" => "Oku",
+	"yazar" => "Hüseyin Tuluk",
+	"sayfa_no" => "121",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "09 Ekim 2015"
+
+);
+
+$kitaplar[96] = array(
+
+	"resim" => "",
+	"isim" => "Risale Okumaları Keşif Yolculukları",
+	"yazar" => "Metin Karabaşoğlu",
+	"sayfa_no" => "160",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "24 Ekim 2015"
+
+);
+
+$kitaplar[97] = array(
+
+	"resim" => "",
+	"isim" => "Dost Ateşi",
+	"yazar" => "Şener Berkman",
+	"sayfa_no" => "542",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "25 Kasım 2015"
+
+);
+
+$kitaplar[98] = array(
+
+	"resim" => "",
+	"isim" => "Benim Yolum",
+	"yazar" => "Christina Daniels",
+	"sayfa_no" => "352",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "21 Ocak 2016"
+
+);
+
+$kitaplar[99] = array(
+
+	"resim" => "",
+	"isim" => "Od",
+	"yazar" => "İskender Pala",
+	"sayfa_no" => "359",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "15 Şubat 2016"
+
+);
+
+$kitaplar[100] = array(
+
+	"resim" => "",
+	"isim" => "Psiko Analist",
+	"yazar" => "John Katzenbach",
+	"sayfa_no" => "491",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "17 Haziran 2016"
+
+);
+
+$kitaplar[101] = array(
+
+	"resim" => "",
+	"isim" => "Nuriye Ana",
+	"yazar" => "Hülya Yakut Üstündağ",
+	"sayfa_no" => "310",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "21 Eylül 2016"
+
+);
+
+$kitaplar[102] = array(
+
+	"resim" => "",
+	"isim" => "İngilizce Dilbilgisi",
+	"yazar" => "Bahire Şerif",
+	"sayfa_no" => "560",
+	"basim_yili" => "?",
+	"bitis_tarihi" => "06 Ekim 2016"
+
+);
---
+++ kitaplar.php
@@ -0,0 +1,19 @@
+<?php
+	include("fonksiyonlar.php");
+	$sayfa = "Okuduğum Kitaplar";
+
+function sayfaIcerigi() {
+?>
+<div class="sutun-12">
+
+	<h2>Okumuş Olduğum Kitaplar</h2>
+
+
+</div> <!-- iceriğin sağ tarafının sonu -->
+
+<?php
+}
+
+tema_sablon( $sayfa );
+
+?>
